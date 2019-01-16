3585237b6ec1d1435d2d7eb155d6640786ed2ad9
rbuzatto <mightyrafa@gmail.com>
2019-01-16 15:11:32 -0200

    add head tag and scrollbar scss

--- components/About.js
+++ components/About.js
@@ -1,9 +1,9 @@
 const About = () => (
     <section className="about">
-        <h2 className="about__header">About</h2>
-        <p className="about__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptatum officia dolorem aperiam? Nulla fugit accusamus perferendis delectus dignissimos velit nisi maiores minus, quibusdam ullam necessitatibus facere rem accusantium omnis?</p>
-        <p className="about__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptatum officia dolorem aperiam? Nulla fugit accusamus perferendis delectus dignissimos velit nisi maiores minus, quibusdam ullam necessitatibus facere rem accusantium omnis?</p>
-        <p className="about__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptatum officia dolorem aperiam? Nulla fugit accusamus perferendis delectus dignissimos velit nisi maiores minus, quibusdam ullam necessitatibus facere rem accusantium omnis?</p>
+        <h2 className="about__header">Who am I?</h2>
+        <p className="about__text">Hi there, my name is Rafael Buzatto de Campos. Once a Civil Engineer, other time a poker player, but now I&apos;m a fullstack developer! In frontend I have a background built up on React and Angular while at the backend in NodeJS. </p>
+        <p className="about__text">I started my journey studying Civil Engineer at Unicamp. Shortly after my graduation I started a new career as successfull professional poker player, where I&apos;ve spent over 10 years in a highly competitive arena playing against some of the toughest players in the world.</p>
+        <p className="about__text">For the last 2 years I switched for a new career at web development, where I found a great passion in it. Started with Java, then moved to Javascript where I learned these exciting tools, React and Angular. I also enjoy working with backend, being able to do authentications and fancy stuff.</p>
     </section>
 )
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import '../styles/index.scss'
+import Head from 'next/head'
 import Main from '../components/Main'
 import About from '../components/About'
 import Skills from '../components/Skills'
 
-const App = () => 
-    <main>
-        <Main />
-        <About />
-        <Skills />
-    </main>
+const App = () =>
+    <div>
+        <Head>	  
+          <title>Rafael Portfolio</title>
+          <meta charset="utf-8"/>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <main>
+            <Main />
+            <About />
+            <Skills />
+        </main>
+    </div>
 
 export default App
