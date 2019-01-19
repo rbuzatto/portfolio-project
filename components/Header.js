7759e66b80dcfa68bc7c2f94f528cdbafd89b268
rbuzatto <mightyrafa@gmail.com>
2019-01-19 02:41:18 -0200

    Create Header Animation

--- components/Header.js
+++ components/Header.js
@@ -1,40 +1,44 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { CSSTransitionGroup } from 'react-transition-group'
 
 class Header extends Component {
-    state = {
-        menuOpen: false
-    }
 
     render() {
-        const { menuOpen } = this.state
+        const { menuOpen } = this.props
 
         return (
         <header className="header">
         <CSSTransitionGroup
           transitionName="nav-animation"
-          transitionEnterTimeout={500}
+          transitionEnterTimeout={300}
           transitionLeaveTimeout={300}>
             {
                 menuOpen && (
-                <nav className="header__nav">
-                    <a className = "header__link"   href="">Home</a>
-                    <a className = "header__link"   href="">About</a>
-                    <a className = "header__link"   href="">Skills</a>
-                    <a className = "header__link"   href="">Contact</a>
-                </nav>
+                <div className="header__nav-box">
+                    <nav className="header__nav">
+                        <a className = "header__link"   href="">Home</a>
+                        <a className = "header__link"   href="">About</a>
+                        <a className = "header__link"   href="">Skills</a>
+                        <a className = "header__link"   href="">Contact</a>
+                    </nav>
+                </div>
                 )
             }
         </CSSTransitionGroup>
         
             
-            <div className="header__menu-box" onClick={() => this.setState(({ menuOpen }) => ({ menuOpen : !menuOpen }))}>
+            <div className="header__menu-box" onClick={this.props.displayMenu}>
                 <div className="header__menu"></div>
             </div>
         </header>
         )
     }
-    
+}
+
+Header.propTypes = {
+    menuOpen: PropTypes.bool.isRequired,
+    displayMenu: PropTypes.func.isRequired,
 }
 
 export default Header
--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,52 @@ import Main from '../components/Main'
 import About from '../components/About'
 import Skills from '../components/Skills'
 
-const App = () =>
-    <div>
-        <Head>	  
-          <title>Rafael Portfolio</title>
-          <meta charSet="utf-8"/>
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
-        <Header />
-        <main>
-            <Main />
-            <About />
-            <Skills />
-        </main>
-    </div>
+import React, { Component } from 'react'
+
+class App extends Component{ 
+    state = {
+        menuOpen: false
+    }
+    
+    displayMenu = () => {
+        this.setState(({ menuOpen }) => ({ menuOpen : !menuOpen }))
+    }
+
+    render() {
+        return(
+        <div>
+            <Head>	  
+            <title>Rafael Portfolio</title>
+            <meta charSet="utf-8"/>
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <Header displayMenu={this.displayMenu} menuOpen={this.state.menuOpen} />
+            <main className={`main ${this.state.menuOpen ? 'main-animate' : ''}`}>
+                <Main />
+                <About />
+                <Skills />
+            </main>
+        </div>
+    )
+    }
+}
+
+// const App = () =>
+//     <div>
+//         <Head>	  
+//           <title>Rafael Portfolio</title>
+//           <meta charSet="utf-8"/>
+//           <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+//           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+//         </Head>
+//         <Header />
+//         <main>
+//             <Main />
+//             <About />
+//             <Skills />
+//         </main>
+//     </div>
+
 
 export default App
