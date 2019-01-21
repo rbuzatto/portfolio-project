398e56d1a514eb824f76d667f023e99e6303bb5f
rbuzatto <mightyrafa@gmail.com>
2019-01-21 03:51:46 -0200

    Footer animation and style

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,30 @@
 import PropTypes from 'prop-types'
+import { contactSvgs } from '../icons/paths'
 
 const Footer = props => {
     const translateClass = props.isMenuOpen ? 'footer-animate' : ''
     return (
-    <footer className={`footer ${translateClass}`}>Hi from footer</footer>
+    <footer className={`footer ${translateClass}`}>
+        <div className="footer__something">
+            <div className="footer__content-box">
+                <div className="footer__curtains footer__curtains--left">&#60;</div>
+                <div className="footer__welcome">Find me</div>
+                <div className="footer__contact-wrapper">
+                    <div className="footer__contact">
+                        { contactSvgs.map((path, idx) => {
+                            return(<div key={idx} className="footer__icon">
+                                { path }
+                            </div>
+                            )
+                            }      
+                        ) 
+                        }
+                    </div>
+                </div>
+                <div className="footer__curtains footer__curtains--right">&#47;&#62;</div>
+            </div>    
+        </div>
+    </footer>
     )
 }
 
