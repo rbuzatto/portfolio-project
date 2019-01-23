4c783851a1225279cd4ca58b54dc3ed53c759a5d
rbuzatto <mightyrafa@gmail.com>
2019-01-23 14:26:20 -0200

    alter footer animation for a row display of icons

--- components/Footer.js
+++ components/Footer.js
@@ -5,12 +5,12 @@ const Footer = props => {
     const translateClass = props.isMenuOpen ? 'footer-animate' : ''
     return (
     <footer className={`footer ${translateClass}`}>
-        <div className="footer__something">
-            <div className="footer__content-box">
+        <div className="footer__content-box">
+            <div className="footer__content">
                 <div className="footer__curtains footer__curtains--left">&#60;</div>
                 <div className="footer__welcome">Find me</div>
-                <div className="footer__contact-wrapper">
-                    <div className="footer__contact">
+                <div className="footer__contact">
+                    {/* <div className="footer__contact"> */}
                         { contactSvgs.map((path, idx) => {
                             return(<div key={idx} className="footer__icon">
                                 { path }
@@ -19,7 +19,7 @@ const Footer = props => {
                             }      
                         ) 
                         }
-                    </div>
+                    {/* </div> */}
                 </div>
                 <div className="footer__curtains footer__curtains--right">&#47;&#62;</div>
             </div>    
