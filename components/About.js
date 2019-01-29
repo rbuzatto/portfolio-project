dbe43e75fc34d487713a74e5c6dffb6b56b12765
rbuzatto <mightyrafa@gmail.com>
2019-01-28 23:29:28 -0200

    Add media querie to about section

--- components/About.js
+++ components/About.js
@@ -7,10 +7,11 @@ const About = () => (
                 <div className="about__title about__title--right">Me.</div>
             </div>
         </div>
-        <h2 className="about__header">Who am I?</h2>
-        <p className="about__text">Hi there, my name is Rafael Buzatto de Campos. Once a Civil Engineer, other time a poker player, but now I&apos;m a fullstack developer! In frontend I have a background built up on React and Angular while at the backend in NodeJS. </p>
-        <p className="about__text">I started my journey studying Civil Engineer at Unicamp. Shortly after my graduation I started a new career as successfull professional poker player, where I&apos;ve spent over 10 years in a highly competitive arena playing against some of the toughest players in the world.</p>
-        <p className="about__text">For the last 2 years I switched for a new career at web development, where I found a great passion in it. Started with Java, then moved to Javascript where I learned these exciting tools, React and Angular. I also enjoy working with backend, being able to do authentications and fancy stuff.</p>
+        <div>
+            <p className="about__text">Hi there, my name is Rafael Buzatto de Campos. Once a Civil Engineer, other time a poker player, but now I&apos;m a fullstack developer! In frontend I have a background built up on React and Angular while at the backend in NodeJS. </p>
+            <p className="about__text">I started my journey studying Civil Engineer at Unicamp. Shortly after my graduation I started a new career as successfull professional poker player, where I&apos;ve spent over 10 years in a highly competitive arena playing against some of the toughest players in the world.</p>
+            <p className="about__text">For the last 2 years I switched for a new career at web development, where I found a great passion in it. Started with Java, then moved to Javascript where I learned these exciting tools, React and Angular. I also enjoy working with backend, being able to do authentications and fancy stuff.</p>
+         </div>
     </section>
 )
 
