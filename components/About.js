9b46f71b9b315d8e32e92549ac35451b560d94e7
rbuzatto <mightyrafa@gmail.com>
2019-01-29 12:08:32 -0200

    include intro to section skills

--- components/About.js
+++ components/About.js
@@ -2,14 +2,7 @@ import Intro from './Intro'
 
 const About = () => (
     <section className="about">
-        {/* <div className="about__intro">
-            <div className="about__wrapper">
-                <div className="about__title about__title--left"><span className="about__title-bg-layout">About</span></div>
-                <div className="about__line"></div>
-                <div className="about__title about__title--right">Me.</div>
-            </div>
-        </div> */}
-        <Intro firstText='About' secondText='me.' />
+        <Intro firstText='About' secondText='me.' hue='96' />
         <div>
             <p className="about__text">Hi there, my name is Rafael Buzatto de Campos. Once a Civil Engineer, other time a poker player, but now I&apos;m a fullstack developer! In frontend I have a background built up on React and Angular while at the backend in NodeJS. </p>
             <p className="about__text">I started my journey studying Civil Engineer at Unicamp. Shortly after my graduation I started a new career as successfull professional poker player, where I&apos;ve spent over 10 years in a highly competitive arena playing against some of the toughest players in the world.</p>
--- components/Skills.js
+++ components/Skills.js
@@ -1,3 +1,5 @@
+import Intro from './Intro'
+
 const inlineStyleDefault = {
     width: '40px',
     height:' 40px',
@@ -12,36 +14,39 @@ import { frontend, backend } from '../icons/paths'
 
 const Skills = () => (
     <section className="skills">
-        <h2 className="skills__title">What Do I Know?</h2>
-        <p className="skills__text">I have some knowledge about some stuff. These stuff are fun.</p>
-
-            <div className="area">
-                <input type="radio" name="area" id="frontend" value="frontend" className="area__option" defaultChecked />
-                <input type="radio" name="area" id="backend" value="backend" className="area__option" />
-                <div className="area__labels">
-                <label className="area__expertise" htmlFor="frontend">frontend</label>
-                <label className="area__expertise" htmlFor="backend">backend</label>
+        <Intro firstText='Skills' secondText='my' reverse={true} hue='210' />
+        <div>
+            <h2 className="skills__title">What Do I Know?</h2>
+            <p className="skills__text">I have some knowledge about some stuff. These stuff are fun.</p>
+            
+                <div className="area">
+                    <input type="radio" name="area" id="frontend" value="frontend" className="area__option" defaultChecked />
+                    <input type="radio" name="area" id="backend" value="backend" className="area__option" />
+                    <div className="area__labels">
+                    <label className="area__expertise" htmlFor="frontend">frontend</label>
+                    <label className="area__expertise" htmlFor="backend">backend</label>
+                    </div>
+            
+                    <div className="cards cards--frontend">
+                        { frontend.map((path, idx) => (
+                            <div key={idx} className="card">
+                                <div className="zoomed"></div>
+                                { path }
+                            </div>
+                        )) }
+                    </div>
+                    
+                    <div className="cards cards--backend">
+                        { backend.map((path, idx) => (
+                            <div key={idx} className="card">
+                                <div className="zoomed"></div>
+                                { path }
+                            </div>
+                        )) 
+                        }
+                    </div>
                 </div>
-
-                <div className="cards cards--frontend">
-                    { frontend.map((path, idx) => (
-                        <div key={idx} className="card">
-                            <div className="zoomed"></div>
-                            { path }
-                        </div>
-                    )) }
-                </div>
-                
-                <div className="cards cards--backend">
-                    { backend.map((path, idx) => (
-                        <div key={idx} className="card">
-                            <div className="zoomed"></div>
-                            { path }
-                        </div>
-                    )) 
-                    }
-                </div>
-            </div>
+        </div>
 
     </section>
 )
