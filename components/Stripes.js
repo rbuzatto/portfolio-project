9f326524caae5b11df197407b91057c10d7f104d
rbuzatto <mightyrafa@gmail.com>
2019-03-31 19:43:39 -0300

    update landing page style

--- components/Stripes.js
+++ components/Stripes.js
@@ -1,24 +1,25 @@
-const Stripes = () => (
+const Stripes = () => {
+    const config = {
+        'line--1': '0,200 150,200 200,90 300,90',
+        'line--2': '100,-10 170,180 300,180',
+        'line--3': '300,200 180,200 215,120 300,120'
+    }
+
+    return (
     <svg width="100%" height="100%" className="stripes" 
         viewBox="0 0 300 300"
         preserveAspectRatio="none"
         >
-    <polyline points="0,200 150,200 200,90 300,90"
-                id="line--1"
+        { Object.entries(config).map( ([key, value]) => (
+            <polyline points={value}
+                id={key}
+                key={key}
                 className="stroke__line"
                 />
-    
-    <polyline points="70,-10 170,180 300,180"
-                id="line--2"
-                className="stroke__line"
-                />
-    
-    <polyline points="300,200 180,200 215,120 300,120"
-                id="line--3"
-                className="stroke__line"
-                />
-    
+            ))
+        }
     </svg>
-)
+    )
+}
 
 export default Stripes
